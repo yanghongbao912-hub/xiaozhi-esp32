ab199b98b49fd4838f021084db806e43501bb6c4
yanghongbao912-hub <yanghongbao912@gmail.com>
2026-08-20 00:42:39 +0800

    s3cam: camera RGB565 QVGA for stable capture

---
+++ main/boards/bread-compact-wifi-s3cam-dualeye/compact_wifi_board_s3cam_dualeye.cc
@@ -0,0 +1,170 @@
+#include "wifi_board.h"
+#include "codecs/no_audio_codec.h"
+#include "display/lcd_display.h"
+#include "system_reset.h"
+#include "application.h"
+#include "button.h"
+#include "config.h"
+#include "mcp_server.h"
+#include "led/single_led.h"
+
+#include <esp_log.h>
+#include <driver/spi_common.h>
+#include <esp_lcd_panel_io.h>
+#include <esp_lcd_panel_ops.h>
+#include <esp_lcd_panel_vendor.h>
+#include <esp_lcd_gc9a01.h>
+
+// GC9D01 init sequence (official Waveshare 0.71inch DualEye TFT_eSPI driver)
+static const gc9a01_lcd_init_cmd_t gc9d01_lcd_init_cmds[] = {
+    //  {cmd, { data }, data_size, delay_ms}
+    {0xFE, (uint8_t[]){}, 0, 0},
+    {0xEF, (uint8_t[]){}, 0, 0},
+    {0x80, (uint8_t[]){0xFF}, 1, 0},
+    {0x81, (uint8_t[]){0xFF}, 1, 0},
+    {0x82, (uint8_t[]){0xFF}, 1, 0},
+    {0x83, (uint8_t[]){0xFF}, 1, 0},
+    {0x84, (uint8_t[]){0xFF}, 1, 0},
+    {0x85, (uint8_t[]){0xFF}, 1, 0},
+    {0x86, (uint8_t[]){0xFF}, 1, 0},
+    {0x87, (uint8_t[]){0xFF}, 1, 0},
+    {0x88, (uint8_t[]){0xFF}, 1, 0},
+    {0x89, (uint8_t[]){0xFF}, 1, 0},
+    {0x8A, (uint8_t[]){0xFF}, 1, 0},
+    {0x8B, (uint8_t[]){0xFF}, 1, 0},
+    {0x8C, (uint8_t[]){0xFF}, 1, 0},
+    {0x8D, (uint8_t[]){0xFF}, 1, 0},
+    {0x8E, (uint8_t[]){0xFF}, 1, 0},
+    {0x8F, (uint8_t[]){0xFF}, 1, 0},
+    {0x3A, (uint8_t[]){0x05}, 1, 0},
+    {0xEC, (uint8_t[]){0x01}, 1, 0},
+    {0x74, (uint8_t[]){0x02, 0x0E, 0x00, 0x00, 0x00, 0x00, 0x00}, 7, 0},
+    {0x98, (uint8_t[]){0x3E}, 1, 0},
+    {0x99, (uint8_t[]){0x3E}, 1, 0},
+    {0xB5, (uint8_t[]){0x0D, 0x0D}, 2, 0},
+    {0x60, (uint8_t[]){0x38, 0x0F, 0x79, 0x67}, 4, 0},
+    {0x61, (uint8_t[]){0x38, 0x11, 0x79, 0x67}, 4, 0},
+    {0x64, (uint8_t[]){0x38, 0x17, 0x71, 0x5F, 0x79, 0x67}, 6, 0},
+    {0x65, (uint8_t[]){0x38, 0x13, 0x71, 0x5B, 0x79, 0x67}, 6, 0},
+    {0x6A, (uint8_t[]){0x00, 0x00}, 2, 0},
+    {0x6C, (uint8_t[]){0x22, 0x02, 0x22, 0x02, 0x22, 0x22, 0x50}, 7, 0},
+    {0x6E, (uint8_t[]){0x03, 0x03, 0x01, 0x01, 0x00, 0x00, 0x0F, 0x0F, 0x0D, 0x0D, 0x0B, 0x0B, 0x09, 0x09, 0x00, 0x00, 0x00, 0x00, 0x0A, 0x0A, 0x0C, 0x0C, 0x0E, 0x0E, 0x10, 0x10, 0x00, 0x00, 0x02, 0x02, 0x04, 0x04}, 32, 0},
+    {0xBF, (uint8_t[]){0x01}, 1, 0},
+    {0xF9, (uint8_t[]){0x40}, 1, 0},
+    {0x9B, (uint8_t[]){0x3B}, 1, 0},
+    {0x93, (uint8_t[]){0x33, 0x7F, 0x00}, 3, 0},
+    {0x7E, (uint8_t[]){0x30}, 1, 0},
+    {0x70, (uint8_t[]){0x0D, 0x02, 0x08, 0x0D, 0x02, 0x08}, 6, 0},
+    {0x71, (uint8_t[]){0x0D, 0x02, 0x08}, 3, 0},
+    {0x91, (uint8_t[]){0x0E, 0x09}, 2, 0},
+    {0xC3, (uint8_t[]){0x19}, 1, 0},
+    {0xC4, (uint8_t[]){0x19}, 1, 0},
+    {0xC9, (uint8_t[]){0x3C}, 1, 0},
+    {0xF0, (uint8_t[]){0x53, 0x15, 0x0A, 0x04, 0x00, 0x3E}, 6, 0},
+    {0xF2, (uint8_t[]){0x53, 0x15, 0x0A, 0x04, 0x00, 0x3A}, 6, 0},
+    {0xF1, (uint8_t[]){0x56, 0xA8, 0x7F, 0x33, 0x34, 0x5F}, 6, 0},
+    {0xF3, (uint8_t[]){0x52, 0xA4, 0x7F, 0x33, 0x34, 0xDF}, 6, 0},
+    {0x36, (uint8_t[]){0x00}, 1, 0},
+    {0x11, (uint8_t[]){}, 0, 200},
+    {0x29, (uint8_t[]){}, 0, 0},
+    {0x2C, (uint8_t[]){}, 0, 0},
+};
+
+#define TAG "CompactWifiBoardS3CamDualEye"
+
+class CompactWifiBoardS3CamDualEye : public WifiBoard {
+private:
+    Button boot_button_;
+    LcdDisplay* display_;
+
+    void InitializeSpi() {
+        spi_bus_config_t buscfg = {};
+        buscfg.mosi_io_num = DISPLAY_MOSI_PIN;
+        buscfg.miso_io_num = GPIO_NUM_NC;
+        buscfg.sclk_io_num = DISPLAY_CLK_PIN;
+        buscfg.quadwp_io_num = GPIO_NUM_NC;
+        buscfg.quadhd_io_num = GPIO_NUM_NC;
+        buscfg.max_transfer_sz = DISPLAY_WIDTH * DISPLAY_HEIGHT * sizeof(uint16_t);
+        ESP_ERROR_CHECK(spi_bus_initialize(SPI3_HOST, &buscfg, SPI_DMA_CH_AUTO));
+    }
+
+    void InitializeLcdDisplay() {
+        esp_lcd_panel_io_handle_t panel_io = nullptr;
+        esp_lcd_panel_handle_t panel = nullptr;
+
+        ESP_LOGD(TAG, "Install panel IO");
+        esp_lcd_panel_io_spi_config_t io_config = {};
+        io_config.cs_gpio_num = DISPLAY_CS_PIN;
+        io_config.dc_gpio_num = DISPLAY_DC_PIN;
+        io_config.spi_mode = DISPLAY_SPI_MODE;
+        io_config.pclk_hz = 20 * 1000 * 1000;
+        io_config.trans_queue_depth = 10;
+        io_config.lcd_cmd_bits = 8;
+        io_config.lcd_param_bits = 8;
+        ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi(SPI3_HOST, &io_config, &panel_io));
+
+        ESP_LOGD(TAG, "Install GC9D01 driver");
+        esp_lcd_panel_dev_config_t panel_config = {};
+        panel_config.reset_gpio_num = DISPLAY_RST_PIN;
+        panel_config.rgb_ele_order = DISPLAY_RGB_ORDER;
+        panel_config.bits_per_pixel = 16;
+        // 必须在创建 panel 之前把 vendor_config(GC9D01 初始化序列) 挂上,
+        // 否则 GC9D01 会被当作默认 GC9A01 初始化, 屏幕无法点亮
+        gc9a01_vendor_config_t gc9d01_vendor_config = {
+            .init_cmds = gc9d01_lcd_init_cmds,
+            .init_cmds_size = sizeof(gc9d01_lcd_init_cmds) / sizeof(gc9a01_lcd_init_cmd_t),
+        };
+        panel_config.vendor_config = &gc9d01_vendor_config;
+        ESP_ERROR_CHECK(esp_lcd_new_panel_gc9a01(panel_io, &panel_config, &panel));
+
+        esp_lcd_panel_reset(panel);
+        esp_lcd_panel_init(panel);
+        esp_lcd_panel_invert_color(panel, DISPLAY_INVERT_COLOR);
+        esp_lcd_panel_swap_xy(panel, DISPLAY_SWAP_XY);
+        esp_lcd_panel_mirror(panel, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y);
+
+        display_ = new SpiLcdDisplay(panel_io, panel,
+                                     DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_OFFSET_X, DISPLAY_OFFSET_Y, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y, DISPLAY_SWAP_XY);
+    }
+
+    void InitializeButtons() {
+        boot_button_.OnClick([this]() {
+            auto& app = Application::GetInstance();
+            if (app.GetDeviceState() == kDeviceStateStarting) {
+                EnterWifiConfigMode();
+                return;
+            }
+            app.ToggleChatState();
+        });
+    }
+
+public:
+    CompactWifiBoardS3CamDualEye() :
+        boot_button_(BOOT_BUTTON_GPIO) {
+        InitializeSpi();
+        InitializeLcdDisplay();
+        InitializeButtons();
+    }
+
+    virtual Led* GetLed() override {
+        static SingleLed led(BUILTIN_LED_GPIO);
+        return &led;
+    }
+
+    virtual AudioCodec* GetAudioCodec() override {
+#ifdef AUDIO_I2S_METHOD_SIMPLEX
+        static NoAudioCodecSimplex audio_codec(AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
+            AUDIO_I2S_SPK_GPIO_BCLK, AUDIO_I2S_SPK_GPIO_LRCK, AUDIO_I2S_SPK_GPIO_DOUT, AUDIO_I2S_MIC_GPIO_SCK, AUDIO_I2S_MIC_GPIO_WS, AUDIO_I2S_MIC_GPIO_DIN);
+#else
+        static NoAudioCodecDuplex audio_codec(AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
+            AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_WS, AUDIO_I2S_GPIO_DOUT, AUDIO_I2S_GPIO_DIN);
+#endif
+        return &audio_codec;
+    }
+
+    virtual Display* GetDisplay() override {
+        return display_;
+    }
+};
+
+DECLARE_BOARD(CompactWifiBoardS3CamDualEye);
